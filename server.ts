600a61c3bb7a72119cc65435ceaea64b2aa5bc41
Talkwondo <ttp@oxidegroup.co.il>
2025-09-19 10:47:10 +0300

    fix

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { handleWebhook, verifyWebhook } from "./src/controllers/webhook";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3980;
 
 app.use(express.json());
 
