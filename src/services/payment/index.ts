f3b7db6d5c25467513bfe86de9df2a4d23df0ba6
Talkwondo <ttp@oxidegroup.co.il>
2026-01-04 08:31:54 +0200

    add logs

--- src/services/payment/index.ts
+++ src/services/payment/index.ts
@@ -70,7 +70,7 @@ const extractGroupTokenFragment = (redirectURL: string) => {
     return {
       groupId,
       token,
-      fragment: `GroupId=${groupId}&Token=${token}`,
+      fragment: `${groupId}&Token=${token}`,
     };
   }
 };
