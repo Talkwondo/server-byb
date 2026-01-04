0db01b13491b92b9fddd3dadeb44d42f20716cad
Talkwondo <ttp@oxidegroup.co.il>
2026-01-04 08:04:36 +0200

    add logs

--- src/services/server/server.modal.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FlowType = void 0;
-var FlowType;
-(function (FlowType) {
-    FlowType["CTB"] = "CTB";
-    FlowType["BTC"] = "BTC";
-})(FlowType || (exports.FlowType = FlowType = {}));
